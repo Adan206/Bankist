b7869906fbe7b153675b0aae3106db58ef0f0a43
Adan Abbi <adan@Adans-MacBook-Air-2.local>
2023-02-23 00:19:39 -0800

    add scroll

--- script.js
+++ script.js
@@ -8,7 +8,10 @@ const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.btn--close-modal');
 const btnsOpenModal = document.querySelectorAll('.btn--show-modal');
 
-const openModal = function () {
+const header = document.querySelector('.header');
+
+const openModal = function (event) {
+  event.preventDefault();
   modal.classList.remove('hidden');
   overlay.classList.remove('hidden');
 };
@@ -18,8 +21,10 @@ const closeModal = function () {
   overlay.classList.add('hidden');
 };
 
-for (let i = 0; i < btnsOpenModal.length; i++)
-  btnsOpenModal[i].addEventListener('click', openModal);
+// for (let i = 0; i < btnsOpenModal.length; i++)
+//   btnsOpenModal[i].addEventListener('click', openModal);
+
+btnsOpenModal.forEach(model => model.addEventListener('click', openModal));
 
 btnCloseModal.addEventListener('click', closeModal);
 overlay.addEventListener('click', closeModal);
@@ -29,3 +34,35 @@ document.addEventListener('keydown', function (e) {
     closeModal();
   }
 });
+
+const message = document.createElement('div');
+message.classList.add('cookie-message');
+
+message.innerHTML = `we use cookies to improve functionality and analytics 
+<button class='btn btn--close-cookie'>Got It!</button>`;
+
+message.style.background = '#37383d';
+message.style.width = '120%';
+
+header.append(message);
+
+document.querySelector('.btn--close-cookie').addEventListener('click', e => {
+  message.remove();
+});
+
+console.log(getComputedStyle(message).height);
+console.log(getComputedStyle(message).backgroundColor);
+
+message.style.height =
+  Number.parseFloat(getComputedStyle(message).height, 10) + 30 + 'px';
+
+const logo = document.querySelector('.nav__logo');
+logo.className; //?
+logo.src; //?
+logo.getAttribute('src'); //?
+logo.getAttribute('designer'); //?
+logo.alt; //?
+logo.alt = 'bank best bank';
+logo.alt; //?
+logo.setAttribute('location', 'California');
+logo.getAttribute('location'); //?
